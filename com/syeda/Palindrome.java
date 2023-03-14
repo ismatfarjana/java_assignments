56e934669c340828b53b78a16be01b1d7b106824
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-03-14 17:40:32 +1100

    palindrome checker

--- com/syeda/Palindrome.java
+++ com/syeda/Palindrome.java
@@ -1,4 +1,26 @@
 package com.syeda;
 
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("------------------");
+        System.out.println("PALINDROME CHECKER");
+        System.out.println("------------------");
+        System.out.println("Enter a word:");
+        String word = input.nextLine();
+        String reversedString = "";
+
+        for (int index = 0; word.length() > index; index++) {
+            reversedString = word.charAt(index) + reversedString;
+        }
+
+        if (reversedString.equals(word)) {
+            System.out.println("PALINDROME");
+        } else {
+            System.out.println("BOOOOO!!! NOT A PALINDROME");
+        }
+        System.out.println("------------------");
+    }
 }
