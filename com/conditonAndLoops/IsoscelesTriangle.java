a70f885cfa47ee830b32ad860bf4efcb85c40f66
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-03-16 14:41:36 +1100

    area of isosceles triangle

---
+++ com/conditonAndLoops/IsoscelesTriangle.java
@@ -0,0 +1,18 @@
+package com.conditonAndLoops;
+
+import java.util.Scanner;
+
+public class IsoscelesTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Area of IsoscelesTriangle calculator");
+        System.out.println("-------------------------");
+        System.out.println("Enter a height: ");
+        float height = (float) input.nextInt();
+        System.out.println("Enter a base: ");
+        float base = (float) input.nextInt();
+        float area = (height * base)/2;
+
+        System.out.println("the area is: " + area);
+    }
+}
