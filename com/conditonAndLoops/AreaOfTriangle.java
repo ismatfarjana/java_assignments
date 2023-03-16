a3259eaa5492106e6d9a678a4c5bac6e2d0baba8
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-03-16 14:17:53 +1100

    area of triangle

--- com/conditonAndLoops/AreaOfTriangle.java
+++ com/conditonAndLoops/AreaOfTriangle.java
@@ -1,2 +1,18 @@
-package com.conditonAndLoops;public class AreaOfTriangle {
+package com.conditonAndLoops;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Area of Triangle calculator");
+        System.out.println("-------------------------");
+        System.out.println("Enter a height: ");
+        float height = (float) input.nextInt();
+        System.out.println("Enter a base: ");
+        float base = (float) input.nextInt();
+        float area = (1.0f/2.0f) * height * base;
+
+        System.out.println("the area is: " + area);
+    }
 }
